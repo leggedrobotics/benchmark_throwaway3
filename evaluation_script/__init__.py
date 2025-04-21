9da1442bb4c6097a4ecb42e768a3c37a0e89d211
tutuna <tutuna@leggedrobotics.com>
2025-04-21 13:03:56 +0200

    deps moved inside eval

--- evaluation_script/__init__.py
+++ evaluation_script/__init__.py
@@ -9,19 +9,20 @@ def install(package):
     #     print(f"❌ Failed to install {package}: {e}")
 
 # Install standard dependencies
-install("numpy==1.24.4")
-install("scipy==1.10.1")
-install("matplotlib==3.7.5")
-install("pyyaml==6.0.2")
-install("tqdm")
-install("argcomplete==3.6.2")
-install("colorama==0.4.6")
-install("pillow==10.4.0")
-install("pykitti")
-install("rosbags==0.9.23")
-install("natsort==8.4.0")
-install("lz4==4.3.3")
-install("zstandard==0.23.0")
+install("numpy")
+install("evo")
+# install("scipy==1.10.1")
+# install("matplotlib==3.7.5")
+# install("pyyaml==6.0.2")
+# install("tqdm")
+# install("argcomplete==3.6.2")
+# install("colorama==0.4.6")
+# install("pillow==10.4.0")
+# install("pykitti")            # Might install additional light deps
+# install("rosbags==0.9.23")
+# install("natsort==8.4.0")
+# install("lz4==4.3.3")
+# install("zstandard==0.23.0")
 
 # Install evo from local wheel inside evaluation_script/deps/
 this_dir = os.path.dirname(__file__)
